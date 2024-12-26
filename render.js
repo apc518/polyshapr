8a9ede391357db538b37001e6ec629fad449eb94
apc518 <apc518@ymail.com>
2024-12-26 01:19:56 -0800

    video rendering and its decent

--- render.js
+++ render.js
@@ -3,7 +3,7 @@ const chunks = [];
 function recordVideo() {
     chunks.length = 0;
     let stream = p5canvas.canvas.captureStream(60);
-    recorder = new MediaRecorder(stream);
+    recorder = new MediaRecorder(stream, { videoBitsPerSecond: 3 * 1024 * 1024 * 8 });
     recorder.ondataavailable = e => {
         if (e.data.size) {
             chunks.push(e.data);
@@ -12,21 +12,19 @@ function recordVideo() {
     recorder.onstop = exportVideo;
     recordVideoBtn.onclick = e => {
         recorder.stop();
-        recordVideoBtn.textContent = 'start recording';
+        recordVideoBtn.textContent = 'Start Recording';
         recordVideoBtn.onclick = recordVideo;
     };
     recorder.start();
-    recordVideoBtn.textContent = 'stop recording';
+    recordVideoBtn.textContent = 'Stop Recording';
 }
 
-function exportVideo(e) {
+function exportVideo() {
     var blob = new Blob(chunks);
-    var vid = document.createElement('video');
-    vid.id = 'recorded'
-    vid.controls = true;
-    vid.src = URL.createObjectURL(blob);
-    document.body.appendChild(vid);
-    vid.play();
+    let downloadElem = document.createElement('a');
+    downloadElem.target = "_blank";
+    downloadElem.href = URL.createObjectURL(blob);
+    downloadElem.click();
 }
 
 recordVideoBtn.onclick = recordVideo;
